3a6050e6c895edda654e398f740463a00ca342ce
tehjeipi <72479272+juangmalta@users.noreply.github.com>
2022-03-10 17:28:55 -0600

    3. cpp and token tests

---
+++ 3_2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#define max 100
+
+using namespace std;
+
+string entrada[max];
+
+int leng(string inp)
+{
+    int largo = 0;
+    for (int i = 0; inp[i] != '\0'; i++)
+    {
+        largo++;
+    }
+    return largo;
+}
+
+void split(string inp, char separador)
+{
+    
+}
+
+int main()
+{
+
+    // string entrada;
+    // cin >> entrada;
+    // string enteros,
+    //     flotantes,
+    //     asignacion = "=",
+    //     suma = "+",
+    //     resta = "-",
+    //     multiplicacion = "*",
+    //     division = "/",
+    //     potencia = "^",
+    //     variables,
+    //     p1 = "(",
+    //     p2 = ")",
+    //     comentarios = "//";
+}
